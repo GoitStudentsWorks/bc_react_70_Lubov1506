cbb37b7479920f34ee3b596f9c805418b08ab134
GoitStudentsWorks <GoitStudentsWorks@users.noreply.github.com>
2024-06-06 20:25:40 +0000

    deploy: dd8fe56b29a90f3a4086f084bafe64058c9bdf83

---
+++ assets/CurrencyTab-CeOOnhDD.js
@@ -0,0 +1,2 @@
+import{j as t,m as a,C as i}from"./index-yo_AdM5v.js";const n=()=>t.jsx(a.div,{initial:{opacity:0},animate:{opacity:1},children:t.jsx(i,{})});export{n as default};
+//# sourceMappingURL=CurrencyTab-CeOOnhDD.js.map
